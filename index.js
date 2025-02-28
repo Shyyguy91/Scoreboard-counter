3fbd25fbc9b2e8023b5a0925e58d441813861096
Shyyguy91 <alex.esp919@gmail.com>
2025-02-27 17:46:47 -0700

    Add initial Css, html and javascript. this includes font syle.

---
+++ index.js
@@ -0,0 +1,33 @@
+let countEl = document.getElementById("count-el-1")
+let count = 0
+let countEl2 = document.getElementById("count-el-2")
+let count2 = 0
+function plusone() {
+    count += 1
+    countEl.innerText = count
+}
+
+function plustwo() {
+    count += 2
+    countEl.innerText = count
+}
+
+function plusthree() {
+    count += 3
+    countEl.innerText = count
+}
+
+function plusone1() {
+    count2 += 1
+    countEl2.innerText = count2
+}
+
+function plustwo2() {
+    count2 += 2
+    countEl2.innerText = count2
+}
+
+function plusthree3() {
+    count2 += 3
+    countEl2.innerText = count2
+}
